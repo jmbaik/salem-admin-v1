859910170eca089ed5ced66b5d67349a0ce4d7fe
jmbaik <famian@daum.net>
2023-04-21 02:07:31 +0900

    s3사용

--- src/views/guide/ComingWayGuide.js
+++ src/views/guide/ComingWayGuide.js
@@ -1,16 +1,100 @@
+import {useEffect, useState} from 'react';
+import {useDropzone} from 'react-dropzone';
+
+const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '10px',
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out',
+};
+
 const thumbsContainer = {
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
     marginTop: 16,
 };
+
 const thumb = {
     display: 'inline-flex',
     borderRadius: 2,
+    border: '1px solid #eaeaea',
+    marginBottom: 8,
+    marginRight: 8,
+    width: 100,
+    height: 100,
+    padding: 4,
+    boxSizing: 'border-box',
 };
 
-const ComingWayGuide = () => {
-    return <div>ComingWayGuide</div>;
+const thumbInner = {
+    display: 'flex',
+    minWidth: 0,
+    overflow: 'hidden',
 };
+
+const img = {
+    display: 'block',
+    width: 'auto',
+    height: '100%',
+};
+
+export function ComingWayGuide(props) {
+    const [files, setFiles] = useState([]);
+    const {getRootProps, getInputProps} = useDropzone({
+        accept: {
+            'image/*': [],
+        },
+        onDrop: (acceptedFiles) => {
+            setFiles(
+                acceptedFiles.map((file) =>
+                    Object.assign(file, {
+                        preview: URL.createObjectURL(file),
+                    }),
+                ),
+            );
+        },
+    });
+
+    const thumbs = files.map((file) => (
+        <div style={thumb} key={file.name}>
+            <div style={thumbInner}>
+                <img
+                    src={file.preview}
+                    alt="preview"
+                    style={img}
+                    // Revoke data uri after image is loaded
+                    onLoad={() => {
+                        URL.revokeObjectURL(file.preview);
+                    }}
+                />
+            </div>
+        </div>
+    ));
+
+    useEffect(() => {
+        // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+        return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
+    }, []);
+
+    return (
+        <section style={baseStyle}>
+            <div {...getRootProps({className: 'dropzone'})}>
+                <input {...getInputProps()} />
+                <p>파일을 여기에 드래그드롭 하거나 클릭하여 파일을 찾아서 첨부하여 주세요.</p>
+            </div>
+            <aside style={thumbsContainer}>{thumbs}</aside>
+        </section>
+    );
+}
 
 export default ComingWayGuide;
--- src/views/guide/NewFamilyGuide.js
+++ src/views/guide/NewFamilyGuide.js
@@ -1,5 +1,79 @@
+import {Alert, Button, Grid} from '@mui/material';
+import AWS from 'aws-sdk';
+import {useState} from 'react';
+
 const NewFamilyGuide = () => {
-    return <div>NewFamilyGuide</div>;
+    const [progress, setProgress] = useState(0);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [showAlert, setShowAlert] = useState(false);
+    const [ext, setExt] = useState('');
+    const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
+    const SECRET_ACCESS_KEY = process.env.REACT_APP_SECRET_ACCESS_KEY;
+    const REGION = process.env.REACT_APP_REGION;
+    const S3_BUCKET = process.env.REACT_APP_S3_BUCKET;
+    console.log(ACCESS_KEY, SECRET_ACCESS_KEY, REGION, S3_BUCKET);
+
+    AWS.config.update({
+        accessKeyId: ACCESS_KEY,
+        secretAccessKey: SECRET_ACCESS_KEY,
+    });
+    const myBucket = new AWS.S3({
+        params: {Bucket: S3_BUCKET},
+        region: REGION,
+    });
+    const handleFileInput = (e) => {
+        const file = e.target.files[0];
+        // console.log(file);
+        const fileExt = file.name.split('.').pop();
+        setExt(fileExt);
+        console.log(fileExt);
+        // if (file.type !== 'image/jpeg' || fileExt !== 'jpg' || fileExt !== 'png') {
+        //     alert('이미지 파일만 업로드 가능합니다.');
+        //     return;
+        // }
+        setProgress(0);
+        setSelectedFile(file);
+    };
+    const uploadFile = (file) => {
+        console.log(file.name);
+        const params = {
+            ACL: 'public-read',
+            Body: file,
+            Bucket: S3_BUCKET,
+            Key: 'upload/' + 'aaaaaa.' + ext,
+        };
+        myBucket
+            .putObject(params)
+            .on('httpUploadProgress', (e) => {
+                setProgress(Math.round((e.loaded / e.total) * 100));
+                setShowAlert(true);
+                setTimeout(() => {
+                    setShowAlert(false);
+                    setSelectedFile(null);
+                }, 3000);
+            })
+            .send((err) => {
+                if (err) console.log(err);
+            });
+    };
+    //metelsoft-store-bucket-v1/upload/kt-giga-접속 단말 정보.png
+    s3: return (
+        <Grid container>
+            <Grid item xs={12}>
+                <h1>File Upload</h1>
+            </Grid>
+            <Grid item xs={12}>
+                {showAlert ? <Alert>업로드 진행률: {progress}%</Alert> : <Alert>파일을 선택해 주세요</Alert>}
+            </Grid>
+            <Grid item xs={12}>
+                <input type="file" onChange={handleFileInput} />
+                {selectedFile ? <Button onClick={() => uploadFile(selectedFile)}>Upload</Button> : null}
+            </Grid>
+            <Grid item xs={12}>
+                <img alt="abc" src={`${process.env.REACT_APP_S3_URL}/upload/aaaaaa.png`} />
+            </Grid>
+        </Grid>
+    );
 };
 
 export default NewFamilyGuide;
