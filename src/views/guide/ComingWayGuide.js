a0bf2e1e2d1b7a6316953f395a6d18bf05e733fa
jmbaik <famian@daum.net>
2023-04-20 12:10:34 +0900

    eslint fix

--- src/views/guide/ComingWayGuide.js
+++ src/views/guide/ComingWayGuide.js
@@ -1,3 +1,14 @@
+const thumbsContainer = {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 16,
+};
+const thumb = {
+    display: 'inline-flex',
+    borderRadius: 
+}
+
 const ComingWayGuide = () => {
     return <div>ComingWayGuide</div>;
 };
